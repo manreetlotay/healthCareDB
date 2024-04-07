6001cbdc8291f90d3a1e5383a22ef8ae9ea1356b
manreetlotay <lotaymk@gmail.com>
2024-04-06 23:39:52 -0400

    set up queries

--- healthcare/src/App.js
+++ healthcare/src/App.js
@@ -17,6 +17,20 @@ import ScheduleTable from './components/ScheduleTable';
 import VaccinationTable from './components/VaccinationTable';
 
 import Query8 from './components/queries/Query8';
+import Query9 from './components/queries/Query9';
+import Query10 from './components/queries/Query10';
+import Query11 from './components/queries/Query11';
+import Query12 from './components/queries/Query12';
+import Query13 from './components/queries/Query13';
+import Query14 from './components/queries/Query14';
+import Query15 from './components/queries/Query15';
+import Query16 from './components/queries/Query16';
+import Query17 from './components/queries/Query17';
+import Query18 from './components/queries/Query18';
+import Query19 from './components/queries/Query19';
+import Query20 from './components/queries/Query20';
+import Query21 from './components/queries/Query21';
+
 
 function App() {
   return (
@@ -41,19 +55,19 @@ function App() {
 
       {/* Queries */}
       <Route path='/query/8' element={<Query8 />} />
-      <Route path='/query/9' element={<Query8 />} />
-      <Route path='/query/10' element={<Query8 />} />
-      <Route path='/query/11' element={<Query8 />} />
-      <Route path='/query/12' element={<Query8 />} />
-      <Route path='/query/13' element={<Query8 />} />
-      <Route path='/query/14' element={<Query8 />} />
-      <Route path='/query/15' element={<Query8 />} />
-      <Route path='/query/16' element={<Query8 />} />
-      <Route path='/query/17' element={<Query8 />} />
-      <Route path='/query/18' element={<Query8 />} />
-      <Route path='/query/19' element={<Query8 />} />
-      <Route path='/query/20' element={<Query8 />} />
-      <Route path='/query/21' element={<Query8 />} />
+      <Route path='/query/9' element={<Query9 />} />
+      <Route path='/query/10' element={<Query10 />} />
+      <Route path='/query/11' element={<Query11 />} />
+      <Route path='/query/12' element={<Query12 />} />
+      <Route path='/query/13' element={<Query13 />} />
+      <Route path='/query/14' element={<Query14 />} />
+      <Route path='/query/15' element={<Query15 />} />
+      <Route path='/query/16' element={<Query16 />} />
+      <Route path='/query/17' element={<Query17 />} />
+      <Route path='/query/18' element={<Query18 />} />
+      <Route path='/query/19' element={<Query19 />} />
+      <Route path='/query/20' element={<Query20 />} />
+      <Route path='/query/21' element={<Query21 />} />
     </Routes>
     </Router>
     </>
--- healthcare/src/components/queries/Query8.js
+++ healthcare/src/components/queries/Query8.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../styles/Queries.css'; // Import the CSS file for styling
+import '../styles/Queries.css'; 
 
 const Query8 = () => {
   const [data, setData] = useState([]);
@@ -26,20 +26,46 @@ const Query8 = () => {
   return (
     <div className="query-container">
       <h1 className="query-title">Result of Query 8</h1>
+      <h3 className="query-description">Get details of all the facilities recorded in the system. Details include facility name, 
+address, city, province, postal-code, phone number, web address, type, capacity, name 
+of the general manager, number of employees currently working at the facility, number 
+of doctors currently working in the facility, and number of nurses currently working in 
+the facility. Results should be displayed sorted in ascending order by province, then by 
+city, then by type, then by number of doctors currently working for the facility.</h3>
       <table className="query-table">
         <thead>
           <tr>
+          {/* <th>Facility Name</th>
+            <th>Address</th>
+            <th>City</th>
+            <th>Province</th>
+            <th>Postal Code</th>
+            <th>Phone Number</th>
+            <th>Web Address</th>
+            <th>Type</th>
+            <th>Capacity</th>
+            <th>General Manager's Name</th>
+            <th>Number of Employees</th>
+            <th>Number of Doctors</th>
+            <th>Number of Nurses</th> */}
+
             <th>Person ID</th>
-            <th>Date of Birth</th>
-            <th>SSN</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Vaccination ID</th>
+            <th>Vaccination Date</th>
+            <th>Vaccination Type</th>
           </tr>
         </thead>
         <tbody>
-          {data.map(person => (
-            <tr key={person.PersonId}>
-              <td>{person.PersonId}</td>
-              <td>{person.DOB}</td>
-              <td>{person.SSN}</td>
+           {data.map(row => (
+            <tr key={row.PersonId}>
+              <td>{row.PersonId}</td>
+              <td>{row.FirstName}</td>
+              <td>{row.LastName}</td>
+              <td>{row.VaccinationId}</td>
+              <td>{row.VaccinationDate}</td>
+              <td>{row.VaccinationType}</td>
             </tr>
           ))}
         </tbody>
