8950785c13cc1a9af992dc11d194c256a63edcd7
unknown <lotaymk@gmail.com>
2024-04-08 12:35:16 -0400

    fix query 14

--- healthcare/src/components/queries/Query14.js
+++ healthcare/src/components/queries/Query14.js
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import '../styles/Queries.css'; 
+import '../styles/Queries.css';
 
 const Query14 = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     fetchData().then((result) => {
-      console.log('Setting data:', result); 
+      console.log('Setting data:', result);
       setData(result);
     });
-  }, []); 
+  }, []);
 
   const fetchData = async () => {
     try {
       const response = await fetch('/query/14');
       const data = await response.json();
-      console.log('Fetched data:', data); 
+      console.log('Fetched data:', data);
       return data;
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -27,26 +27,28 @@ const Query14 = () => {
     <div className="query-container">
       <h1 className="query-title">Query 14</h1>
       <h3 className="query-description">For a given facility with FacilityId = 6, generate a list of all the employees who have at least three
-secondary residences and who have been on schedule to work in the last four weeks, displayed in ascending order by role, then by the number of 
-secondary residences.</h3>
+        secondary residences and who have been on schedule to work in the last four weeks, displayed in ascending order by role, then by the number of
+        secondary residences.</h3>
       <table className="query-table">
         <thead>
           <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Role of Employee</th>
-          <th>Number of Secondary Residences</th>
+            <th>Employee Id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Role of Employee</th>
+            <th>Number of Secondary Residences</th>
           </tr>
         </thead>
         <tbody>
-        {data.map(row => (
-        <tr>
-          <td>{row.FirstName}</td>
-          <td>{row.Lastname}</td>
-          <td>{row.EmployeeRole}</td>
-          <td>{row.number_of_secondary_residences}</td>
-        </tr>
-))}
+          {data.map(row => (
+            <tr>
+              <td>{row.EmployeeId}</td>
+              <td>{row.FirstName}</td>
+              <td>{row.LastName}</td>
+              <td>{row.EmployeeRole}</td>
+              <td>{row.number_of_secondary_residences}</td>
+            </tr>
+          ))}
 
         </tbody>
       </table>
